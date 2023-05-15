0bebc2ee524c87d09f5929d94edcd3649ae9e6d3
Muhammad Amr <midoamr20@gmail.com>
2023-05-16 02:58:03 +0300

    >

---
+++ AES_encryption/SubBytes.v
@@ -0,0 +1,12 @@
+`include "Sbox.v"
+module SubBytes (
+    input [127:0] InState ,
+    output [127:0] OutState
+);
+    genvar i  ;
+    generate
+        for (i = 0  ; i < 128 ; i = i + 8) begin : Sub_Bytes
+            Sbox uut(InState[i+7 : i],OutState[i+7:i]);
+        end
+    endgenerate
+endmodule
---
+++ AES_encryption/SubBytes_tb.v
@@ -0,0 +1,16 @@
+`include "SubBytes.v"
+module SubBytes_tb;
+    reg [127:0] in ;
+    wire [127:0] out;
+    SubBytes  uut(in,out);
+    initial begin
+        $dumpfile("SubBytes_tb.vcd");
+        $dumpvars(0,SubBytes_tb);
+        $display ("time\t input \t\t\t\t\t\t\t\t  output");
+        $monitor ("%g\t %h\t\t %h",$time,in,out);
+        in = 128'h193DE3BEA0F4E22B9AC68D2AE9F84808;
+        #10
+        $finish;
+    end
+endmodule
+//D42711AEE0BF98F1B8B45DE51E415230
